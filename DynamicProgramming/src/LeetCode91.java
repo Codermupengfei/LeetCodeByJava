9bb5971c03a652566370fbfc5b508fa96325ce44
OovEver <mupengfei6688@163.com>
2017-09-06 19:52:44 +0800

    更新LeetCode91题

---
+++ DynamicProgramming/src/LeetCode91.java
@@ -0,0 +1,28 @@
+/**
+ * Created by OovEver on 2017/9/6.
+ */
+public class LeetCode91 {
+    public static int numDecodings(String s) {
+        int n = s.length();
+        if (n == 0) {
+            return 0;
+        }
+        int dp[] = new int[n + 1];
+        dp[n] = 1;
+        dp[n - 1] = s.charAt(n - 1) == '0' ? 0 : 1;
+        for(int i=n-2;i>=0;i--) {
+            if (s.charAt(i) == '0') {
+                continue;
+            } else {
+                dp[i] = (Integer.parseInt(s.substring(i, i + 2)) <= 26) ? dp[i + 1] + dp[i + 2] : dp[i + 1];
+            }
+        }
+
+        return dp[0];
+    }
+
+    public static void main(String[] args) {
+        String s = "";
+        System.out.println(numDecodings(s));
+    }
+}
