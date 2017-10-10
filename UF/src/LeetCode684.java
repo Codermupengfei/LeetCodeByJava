da39dde5f9ecdef193e7774186f5333ac78e8a66
OovEver <mupengfei6688@163.com>
2017-10-10 20:51:11 +0800

    更新684题

---
+++ UF/src/LeetCode684.java
@@ -0,0 +1,25 @@
+/**
+ * Created by OovEver on 2017/10/10.
+ */
+public class LeetCode684 {
+    public int[] findRedundantConnection(int[][] edges) {
+        int []parent=new int[2001];
+        for(int i=0;i<parent.length;i++){
+            parent[i]=i;
+        }
+        for(int []edge:edges){
+            int f=edge[0];
+            int t=edge[1];
+            if(find(parent,f)==find(parent,t))
+                return edge;
+            else parent[find(parent,f)]=find(parent,t);
+        }
+        return new int[2];
+    }
+    private int find(int []parent,int f){
+        if (f != parent[f]) {
+            parent[f] = find(parent, parent[f]);
+        }
+        return parent[f];
+    }
+}
